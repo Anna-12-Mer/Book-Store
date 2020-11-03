c1dd0a583ab1f2ea00cbd13462f8adeafdf208c0
Anna-12-Mer <mammeramna@gmail.Com>
2020-11-03 14:10:23 +0100

    add some updates

---
+++ backEnd/routes/books.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var bodyPerser = require('body-porser');
+
+router.use(bodyPerser.urlencoded({ extended: false }));
+router.use(bodyPerser.json());
+
+var async = require('async');
+
+// Add a book to the store 
+router.post('/addBook', function (req, res) {
+    console.log(req.body);
+
+});
+module.exports = router;
