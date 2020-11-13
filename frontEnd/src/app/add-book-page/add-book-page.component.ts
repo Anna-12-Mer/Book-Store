c5b81dd1ba11e98ffa7e37b239320a30dfe5a843
Anna-12-Mer <mammeramna@gmail.Com>
2020-11-13 21:34:16 +0100

    add some updates

--- frontEnd/src/app/add-book-page/add-book-page.component.ts
+++ frontEnd/src/app/add-book-page/add-book-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book-page',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-book-page.component.css']
 })
 export class AddBookPageComponent implements OnInit {
+  addBookForm : FormBuilder;
 
   constructor() { }
 
